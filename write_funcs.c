6280e62491112869d8339dc5ef30eb1785da194d
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-04-18 17:27:12 +0300

    new change

--- write_funcs.c
+++ write_funcs.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: the character to print the stdout
+ * _putchar - writes the character c to stdout.
+ * @c: the character to print the stdout.
  *
  * Return: on success 1
  * on error, -1 is returned and errno is set appropriately
