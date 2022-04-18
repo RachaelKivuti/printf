6368b34672173701f68b9012337c92e9a8b9c587
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-04-18 16:26:59 +0300

    changes made

--- _putchar.c
+++ _putchar.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <unistd.h>
 
 /**
@@ -6,8 +7,36 @@
  *
  * Return: on success 1
  * on error, -1 is returned and errno is set appropriately
+ *Description: _putchar uses a local buffer of 1024 to call write
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ * Return: number of chars writte
+ */
+int _puts(char *str)
+{
+	register int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
 }
